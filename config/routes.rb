e9be7b6465423a7420aa0917296bf10365419e97
Sara Stanton <sara.mpa13@gmail.com>
2018-07-25 07:37:46 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'students/:id', to: 'students#show'
-  # resources :students, only: :index
+  resources :students, only: :index
+  resources :students, only: :show
 end
